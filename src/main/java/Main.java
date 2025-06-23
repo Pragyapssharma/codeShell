bcde26d25c62909761d0d102d45ed2b618a8a192
pragya sharma <pragyasharma1209@gmail.com>
2025-06-23 15:21:55 +0530

    stdout code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -172,10 +172,10 @@ public class Main {
     	Pattern pattern = Pattern.compile("^(.*?)\\s*(?:1?>|>)\\s*(.*?)$");
         Matcher matcher = pattern.matcher(input);
         
-        if (matcher.matches()) {
-            System.out.println("group(1): " + matcher.group(1)); // echo Hello World
-            System.out.println("group(2): " + matcher.group(2)); // hello.txt
-        }
+//        if (matcher.matches()) {
+//            System.out.println("group(1): " + matcher.group(1)); // echo Hello World
+//            System.out.println("group(2): " + matcher.group(2)); // hello.txt
+//        }
 
         if (!matcher.matches()) {
             System.out.println("Invalid redirection syntax.");
